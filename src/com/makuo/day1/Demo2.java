6c49d36cb544f35caddd5021577dd69f4f3de387
makuo <904578177@qq.com>
2022-08-30 21:57:42 +0800

    添加了Demo2

---
+++ src/com/makuo/day1/Demo2.java
@@ -0,0 +1,13 @@
+package com.makuo.day1;
+
+import java.util.Scanner;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("请输入数字：");
+        int n1= scanner.nextInt();
+
+        System.out.println("n1: "+ n1);
+    }
+}
