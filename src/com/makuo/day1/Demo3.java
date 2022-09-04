8f05b4ef92d48f90560f7e8fafb1bfd0c9fc115b
makuo <904578177@qq.com>
2022-09-05 07:33:07 +0800

    add day2

--- src/com/makuo/day1/Demo3.java
+++ src/com/makuo/day1/Demo3.java
@@ -3,8 +3,42 @@ package com.makuo.day1;
 public class Demo3 {
 
     public static void main(String[] args) {
-        System.out.println("Demo3");
-        System.out.println("2");
+//        System.out.println("Demo3");
+//        System.out.println("2");
+//
+//
+//
+//
+//        String a[]={"aa","bb"};
+//
+//
+//        String b=null;
+//        System.out.println(b);
+
+
+
+
+
+
+    char  a='A';
+
+    char abc[]=new char[26];
+
+    int i=0;
+
+    while (i< abc.length){
+
+        abc[i]=(char)(a+i);
+        System.out.println((char) (a+i));
+        i++;
+    }
+
+
+//        for (char j:abc) {
+//            System.out.println(j);
+//        }
+
+
 
     }
 }
---
+++ src/com/makuo/day1/Demo5.java
@@ -0,0 +1,22 @@
+package com.makuo.day1;
+
+public class Demo5 {
+
+    public static void main(String[] args) {
+
+        // x是int类型的一维数字，y是int类型的二维数组
+        int[] x, y[];
+
+
+        char a = 'x';
+        String s = Character.toString(a);
+        String strs[] = {"x", "y",s};
+
+        for (String i:strs) {
+            System.out.println(i);
+        }
+
+
+    }
+
+}
