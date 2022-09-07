ea538ad3341d98fe4ae61d47a1eb9434bbed8b5c
ma_kuo_ext <ma_kuo_ext@wuxiapptec.com>
2022-09-07 16:45:41 +0800

    add day2.Test.class

---
+++ src/com/makuo/day1/Demo4.java
@@ -0,0 +1,16 @@
+package com.makuo.day1;
+
+public class Demo4 {
+    public static void main(String[] args) {
+
+        double[] hens = { 1, 3, 4, 6, 7 };
+
+        for (double d : hens) {
+            System.out.println((int) d);
+        }
+
+        int ss[] = new int[10];
+        System.out.println(ss[0]);
+        System.out.println(ss[1]);
+    }
+}
