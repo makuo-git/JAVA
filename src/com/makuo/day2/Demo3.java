92043e2a8222c9a319e28df1db461e00e50aae37
makuo <904578177@qq.com>
2022-09-11 21:36:35 +0800

    all

---
+++ src/com/makuo/day2/Demo3.java
@@ -0,0 +1,11 @@
+package com.makuo.day2;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        int a=1;
+        int b=a;
+        a=3;
+
+        System.out.println(b);
+    }
+}
